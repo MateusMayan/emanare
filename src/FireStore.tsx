48d74f646700acb2a47ff3a62e6512b625ddec7e
MateusMayan <mateusmayan@icloud.com>
2024-01-26 18:28:17 -0300

    feat: added database queries in FireStore

--- src/FireStore.tsx
+++ src/FireStore.tsx
@@ -1,22 +1,45 @@
 import React from 'react'
-// Create a reference to the cities collection
 import { db} from './FirebaseConfig'
-import { collection, query, where } from "firebase/firestore";
-import { doc, getDocs } from "firebase/firestore";
+import { collection, query, where,getDocs, documentId } from "firebase/firestore";
 
 const FireStore = () => {
-// Add a new document in collection "cities"
+const userId = "XgOHnfZOlmkPl6qP5o79"
 
 React.useEffect(() => {
-  async function editarDocumento() { 
+  async function buscarClientPedido() { 
+    //Criando coleções de pedidos, clientes e estoque
     const pedidosRef = collection(db, "pedidos");
-    const q = query(pedidosRef, where("statusPedido", "==", 'Em Processamento'));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    })
-  }
-  editarDocumento()
+    const clientPedidoRef = collection(db, 'cliente')
+    const estoqueRef = collection(db, 'estoque')
+    //Query de busca do cliente pelo userId 
+    const query1 = query(clientPedidoRef, where(documentId(), '==', userId))
+    const querySnapshot1 = await getDocs(query1);
+    querySnapshot1.forEach(async (doc) => {
+      console.log(doc.id, " => ", doc.data())
+      
+    //Buscando Os Pedidos do cliente logado
+    const query2 = query(pedidosRef)
+    const querySnapshot2 = await getDocs(query2);
+    querySnapshot2.forEach(async (doc) => {
+      if(doc.data().idCliente.id === userId){
+      console.log('Pedido:', doc.data());
+      console.log('Cliente:', doc.data().idCliente.id)
+      console.log('Itens:', doc.data().itens)
+
+      //Buscando os itens do pedido
+      doc.data().itens.map(async (item : any)=> {
+        const query3 = query(estoqueRef, where(documentId(), '==', item.idProduto))
+        const querySnapshot3 = await getDocs(query3)
+        querySnapshot3.forEach(async(doc) => {
+          console.log(doc.data())
+        })
+      })
+    };
+      })
+    
+  })}
+
+  buscarClientPedido()
 }, [])
 
 
