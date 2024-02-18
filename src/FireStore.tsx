17fe910bc4b646e8d35d6434e724f9a44d40d6e3
MateusMayan <mateusmayan@icloud.com>
2024-02-18 12:44:40 -0300

    feat(depoimentos): increases Depoimentos logic
    Create logic to search for orders with testimonials and score is greater than 4

--- src/FireStore.tsx
+++ src/FireStore.tsx
@@ -1,49 +1,59 @@
-import React from 'react'
-import {db} from './FirebaseConfig'
-import {collection, query, where,getDocs, documentId } from "firebase/firestore";
+import React from 'react';
+import { db } from './FirebaseConfig';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  documentId,
+} from 'firebase/firestore';
 
 const FireStore = () => {
-const userId = "XgOHnfZOlmkPl6qP5o79"
+  const userId = 'XgOHnfZOlmkPl6qP5o79';
 
-React.useEffect(() => {
-  async function buscarClientPedido() { 
-    //Criando coleções de pedidos, clientes e estoque
-    const pedidosRef = collection(db, "pedidos");
-    const clientPedidoRef = collection(db, 'cliente')
-    const estoqueRef = collection(db, 'estoque')
-    //Query de busca do cliente pelo userId 
-    const query1 = query(clientPedidoRef, where(documentId(), '==', userId))
-    const querySnapshot1 = await getDocs(query1);
-    querySnapshot1.forEach(async (doc) => {
-      console.log(doc.id, " => ", doc.data())
-      
-    //Buscando Os Pedidos do cliente logado
-    const query2 = query(pedidosRef)
-    const querySnapshot2 = await getDocs(query2);
-    querySnapshot2.forEach(async (doc) => {
-      if(doc.data().idCliente.id === userId){
-      console.log('Pedido:', doc.data());
-      console.log('Cliente:', doc.data().idCliente.id)
-      console.log('Itens:', doc.data().itens)
+  React.useEffect(() => {
+    async function buscarClientPedido() {
+      //Criando coleções de pedidos, clientes e estoque
+      const pedidosRef = collection(db, 'pedidos');
+      const clientPedidoRef = collection(db, 'cliente');
+      const estoqueRef = collection(db, 'estoque');
+      //Query de busca do cliente pelo userId
+      const query1 = query(clientPedidoRef, where(documentId(), '==', userId));
+      const querySnapshot1 = await getDocs(query1);
+      querySnapshot1.forEach(async (doc) => {
+        console.log(doc.id, ' => ', doc.data());
 
-      //Buscando os itens do pedido
-      doc.data().itens.map(async (item : any)=> {
-        const query3 = query(estoqueRef, where(documentId(), '==', item.idProduto))
-        const querySnapshot3 = await getDocs(query3)
-        querySnapshot3.forEach(async(doc) => {
-          console.log(doc.data())
-        })
-      })
-    };
-      })
-    
-  })}
+        //Buscando Os Pedidos do cliente logado
+        const query2 = query(pedidosRef);
+        const querySnapshot2 = await getDocs(query2);
+        querySnapshot2.forEach(async (doc) => {
+          if (doc.data().idCliente.id === userId) {
+            console.log('Pedido:', doc.data());
+            console.log('Cliente:', doc.data().idCliente.id);
+            console.log('Itens:', doc.data().itens);
 
-  buscarClientPedido()
-}, [])
+            //Buscando os itens do pedido
+            doc.data().itens.map(async (item: any) => {
+              const query3 = query(
+                estoqueRef,
+                where(documentId(), '==', item.idProduto),
+              );
+              const querySnapshot3 = await getDocs(query3);
+              querySnapshot3.forEach(async (doc) => {
+                console.log(doc.data());
+              });
+            });
+          }
+        });
+      });
+    }
 
+    buscarClientPedido();
+  }, []);
 
-  return <div style={{font: 'var(--font-1-xs)'}}> Testando FireStore... </div>
-}
+  return (
+    <div style={{ font: 'var(--font-1-xs)' }}> Testando FireStore... </div>
+  );
+};
 
-export default FireStore
+export default FireStore;
--- src/Home/Depoimentos.tsx
+++ src/Home/Depoimentos.tsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Depoimentos.module.css';
 import { ReactComponent as Heart } from '../Assets/icons/chat-heart 1.svg';
+import { ReactComponent as Star1 } from '../Assets/icons/star-fill 2.svg';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from '../FirebaseConfig';
+
+interface Depoimento {
+  idDocumento: string;
+  nota: number;
+  descricao: string;
+  nomeCliente: string;
+  estadoCliente: string;
+  cidadeCliente: string;
+  [prop: string]: any;
+}
 
 const Depoimentos = () => {
+  const [depoimentos, setDepoimentos] = useState<Depoimento[]>([]);
+
+  useEffect(() => {
+    const fetchDepoimentos = async () => {
+      const PedidosRef = collection(db, 'pedidos');
+      const query1 = query(PedidosRef, where('depoimento', '!=', null));
+      const querySnapshot = await getDocs(query1);
+      const fetchedDepoimentos = await Promise.all(
+        querySnapshot.docs.map(async (document) => {
+          const depoimentoData = document.data().depoimento;
+          const idCliente = document.data().idCliente.id;
+          if (depoimentoData && idCliente) {
+            const ClientDoc = await getDoc(doc(db, 'cliente', idCliente));
+            if (ClientDoc.exists()) {
+              const data = ClientDoc.data();
+              if (data) {
+                return {
+                  idDocumento: document.id,
+                  nota: depoimentoData.nota,
+                  descricao: depoimentoData.descricao,
+                  nomeCliente: data.nome,
+                  estadoCliente: data.endereco.estado,
+                  cidadeCliente: data.endereco.cidade,
+                };
+              }
+            }
+          }
+          return null;
+        }),
+      );
+      const depoimentosValidos = fetchedDepoimentos.filter(
+        (depoimento) => depoimento !== null,
+      ) as Depoimento[];
+      setDepoimentos(depoimentosValidos.slice(0, 3));
+    };
+    fetchDepoimentos();
+  });
+
   return (
     <div className={styles.Container}>
       <div className={styles.DepoimentosTitle}>
@@ -10,6 +68,24 @@ const Depoimentos = () => {
           Depoimentos <span> dos nossos clientes </span>{' '}
         </h3>
         <Heart />
+      </div>
+      <div className={styles.Depoimentos}>
+        {depoimentos.map((doc) => (
+          <div key={doc.idDocumento} className={styles.Depoimento}>
+            <div className={styles.TitleDepoimento}>
+              <h1>{doc.nomeCliente + ','}</h1>
+              <h2>{doc.cidadeCliente + ', ' + doc.estadoCliente}</h2>
+            </div>
+            <div className={styles.DescDepoimento}>
+              <p>{doc.descricao}</p>
+            </div>
+            <div className={styles.notaDepoimento}>
+              {Array.from({ length: doc.nota }).map((_, index) => (
+                <Star1 key={index} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
